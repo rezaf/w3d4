f4f36f3a06a6fe978ec49cf877ddc2b0f01d0fad
Reza Farajian <reza.farajian@gmail.com>
2014-10-23 17:57:10 -0700

    final commit, everything functioning, Question#ar_results

--- PollsProject/app/models/question.rb
+++ PollsProject/app/models/question.rb
@@ -22,54 +22,57 @@ class Question < ActiveRecord::Base
     source: :responses
   )
   
-  # def results
-  #   result = Hash.new(0)
-  #   answer_choices = self.answer_choices.includes(:responses)
-  #
-  #   answer_choices.each do |an_c|
-  #     result[an_c.text] = an_c.responses.length
-  #   end
-  #
-  #   result
-  # end
+  def pre_results
+    result = Hash.new(0)
+    answer_choices = self.answer_choices.includes(:responses)
+
+    answer_choices.each do |an_c|
+      result[an_c.text] = an_c.responses.length
+    end
+
+    result
+  end
   
-  # def results
-  #  answer_choices = AnswerChoice.find_by_sql([
-  #     "SELECT
-  #       answer_choices.*, COUNT(*) AS response_count
-  #     FROM
-  #       answer_choices
-  #     LEFT OUTER JOIN
-  #       responses
-  #     ON
-  #       answer_choices.id = responses.answer_choice_id
-  #     WHERE
-  #       answer_choices.question_id = ?
-  #     GROUP BY
-  #       answer_choice_id", self.id
-  #   ])
-  #
-  #   counts = Hash.new(0)
-  #
-  #   answer_choices.each do |ans_c|
-  #     counts[ans_c.text] = ans_c.response_count
-  #   end
-  #
-  #   # p answer_choices
-  #   # p counts
-  #   counts
-  # end
-  #
   def results
-    ok = AnswerChoice.select(self.answer_choices).joins(
-      "LEFT OUTER JOIN 
-      responses 
-      ON answer_choices.id = responses.answer_choice_id"
-    ).where('self.answer_choices.question_id = ?', self.id
-    )
+   answer_choices = AnswerChoice.find_by_sql([
+      "SELECT
+        answer_choices.*, COUNT(*) AS response_count
+      FROM
+        answer_choices
+      LEFT OUTER JOIN
+        responses
+      ON
+        answer_choices.id = responses.answer_choice_id
+      WHERE
+        answer_choices.question_id = ?
+      GROUP BY
+        answer_choice_id", self.id
+    ])
+
+    counts = Hash.new(0)
+
+    answer_choices.each do |ans_c|
+      counts[ans_c.text] = ans_c.response_count
+    end
     
-    p ok
+    counts
+  end
+
+  
+  def ar_results
+    great_results = self
+      .answer_choices
+      .select("answer_choices.*, COUNT(*) AS response_count")
+      .joins("LEFT OUTER JOIN responses 
+              ON answer_choices.id = responses.answer_choice_id")
+      .where('answer_choices.question_id = ?', self.id)
+      .group("id")
     
+    counts = Hash.new(0)
+      
+    great_results.each { |g_r| counts[g_r.text] = g_r.response_count }
+    
+    counts
   end
   
 end
